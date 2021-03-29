f9392dd0e29a1a00ff5e51459af96c7ea3e481ca
yurikrupnik <krupnik.yuri@gmail.com>
2021-03-29 18:41:16 +0300

    added next 2 package

---
+++ packages/next2/pages/forget-password.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// eslint-disable-next-line
+const ForgetPassword = () => {
+    return <div>forget-password</div>;
+};
+
+ForgetPassword.propTypes = {};
+
+export default ForgetPassword;
